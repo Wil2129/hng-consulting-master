add15665fd24fc56d9a9b48359e19cd0d9795127
Teddy BAHA <michael_takeshi@yahoo.com>
2019-09-21 06:45:50 +0100

    no console.log

--- index.php
+++ index.php
@@ -48,7 +48,6 @@ include('common/functions.php');
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.responseText);
                     document.write(this.responseText);
                     // window.location.href = "signup.php";
                 }
--- signup.php
+++ signup.php
@@ -50,7 +50,6 @@ include('common/functions.php');
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.responseText);
                     document.write(this.responseText);
                     // window.location.href = "index.php";
                 }
